85fd7c1bf89b07904794c681fb44c2676ad228d4
Domguia-Simo <domguiasimoulrich@gmail.com>
2023-05-24 03:59:58 +0200

    Beginning

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+// http.createServer(function(request,respond){
+//     respond.writeHead(200,{'Content-Type':'text/plain'})
+//     respond.end("Domguia Simo Ulich")
+// }).listen(8081);
+
+console.log('My server is running on port number 8081')
+// console.log(http)
