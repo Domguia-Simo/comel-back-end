d213a477e8cab529e3bf7786561e8cd6255b186e
andersonKamsong <kamsonganderson39@gmail.com>
2024-01-07 21:39:57 +0100

    work on election

--- src/routes/election.routes.js
+++ src/routes/election.routes.js
@@ -1,9 +1,15 @@
 
-const { getElections } = require("../controllers/election.controller");
+const { getElections, createElection, startElection, closeElection } = require("../controllers/election.controller");
 
 const express = require("express")
 const router = express.Router()
 
+router.get("/",(req ,res)=>{
+    return res.send("Welcome to my node revision election")
+})
 router.get("/getElections", getElections)
+router.get("/createElection", createElection)
+router.get("/closeElection", closeElection)
+router.get("/startElection", startElection)
 
 module.exports = router;
